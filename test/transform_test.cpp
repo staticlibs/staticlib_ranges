6b7670e16edb34c7433906570c006db0fc8bd84b
alex <alex@staticlibs.net>
2015-02-18 20:39:15 +0000

    tests cleanup

--- test/transform_test.cpp
+++ test/transform_test.cpp
@@ -10,24 +10,25 @@
 #include <list>
 #include <memory>
 #include <sstream>
+#include <iostream>
 
 #include "domain_classes.hpp"
 #include "staticlib/ranges.hpp"
 
 namespace { // anonymous
 
-namespace mv = staticlib::ranges;
+namespace sit = staticlib::ranges;
 
 void test_vector() {
     auto vec = std::vector<std::unique_ptr<MyInt>>{};
     vec.emplace_back(new MyInt(40));
     vec.emplace_back(new MyInt(41));
     
-    auto range = mv::transform(vec, [](std::unique_ptr<MyInt> el) {
+    auto range = sit::transform(vec, [](std::unique_ptr<MyInt> el) {
         return std::unique_ptr<MyStr>(new MyStr(to_string(el->get_int() - 10)));
     });
 
-    auto res = mv::emplace_to_vector(range);
+    auto res = sit::emplace_to_vector(range);
 
     assert(2 == res.size());
     assert("30" == res[0]->get_str());
@@ -44,17 +45,17 @@ void test_range() {
     vec2.emplace_back(new MyStr("53"));
     vec2.emplace_back(new MyStr("54"));
     
-    auto range1 = mv::transform(vec1, [](std::unique_ptr<MyInt> el) {
+    auto range1 = sit::transform(vec1, [](std::unique_ptr<MyInt> el) {
         return std::unique_ptr<MyStr>(new MyStr(to_string(el->get_int() + 10)));
     });
-    auto range2 = mv::concat(range1, vec2);
-    auto range3 = mv::filter(range2, [](std::unique_ptr<MyStr>& el) {
+    auto range2 = sit::concat(range1, vec2);
+    auto range3 = sit::filter(range2, [](std::unique_ptr<MyStr>& el) {
         return "52" != el->get_str();
-    }, mv::ignore_offcast<std::unique_ptr<MyStr>>);
-    auto range4 = mv::transform(range3, [](std::unique_ptr<MyStr> el) {
+    }, sit::ignore_offcast<std::unique_ptr<MyStr>>);
+    auto range4 = sit::transform(range3, [](std::unique_ptr<MyStr> el) {
         return std::unique_ptr<MyStr>(new MyStr(el->get_str() + "_42"));
     });
-    auto res = mv::emplace_to_vector(range4);
+    auto res = sit::emplace_to_vector(range4);
 
     assert(4 == res.size());
     assert("50_42" == res[0]->get_str());
